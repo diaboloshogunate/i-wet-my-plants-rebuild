cdf49f973339a53ebcac4086d3538bf61dc1c728
Robert Parker <diaboloshogunate@gmail.com>
2019-12-14 16:28:14 -0800

    rock, puddle and flower

---
+++ Assets/Scripts/Plant.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class Plant : MonoBehaviour
+{
+    private Animator animator;
+    [SerializeField] private int level = 1;
+    [SerializeField] private int maxLevel = 1;
+    [SerializeField] private float consumption = 5f;
+
+    private void Start()
+    {
+        this.animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player") && this.level < this.maxLevel)
+        {
+            this.Water(other.gameObject.GetComponent<Ball>());
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player") && this.level < this.maxLevel)
+        {
+            this.Water(other.gameObject.GetComponent<Ball>());
+        }
+    }
+
+    private void Water(Ball player)
+    {
+        player.UseWater(this.consumption);
+        this.level = Mathf.Clamp(this.level + 1, 0, this.maxLevel);
+        this.animator.SetInteger("Level", this.level);
+    }
+}
---
+++ Assets/Scripts/WaterSource.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class WaterSource : MonoBehaviour
+{
+    private bool hasBeenUsed = false;
+    private Animator animator;
+    [SerializeField] private float amount = 10f;
+    
+    void Start()
+    {
+        this.animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!this.hasBeenUsed && other.gameObject.CompareTag("Player"))
+        {
+            this.hasBeenUsed = true;
+            other.gameObject.GetComponent<Ball>().UseWater(-1*this.amount);
+            this.animator.SetTrigger("Dry");
+        }
+    }
+}
