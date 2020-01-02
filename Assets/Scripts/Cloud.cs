6da8e950bb1c0b5e33fdce578f5dd36b64e1fd6d
Robert Parker <diaboloshogunate@gmail.com>
2020-01-02 07:15:28 -0800

    cloud spawns puddles

--- Assets/Scripts/Cloud.cs
+++ Assets/Scripts/Cloud.cs
@@ -10,11 +10,14 @@ public class Cloud : MonoBehaviour
 {
     private Animator anim;
     [SerializeField] private GameObject puddle;
+    [SerializeField] private Transform puddleSpawnPosition;
     [SerializeField] private Vector2 spawnXBounds;
     [SerializeField] private Vector2 spawnYBounds;
     [SerializeField] private float minTargetDistance = 2f;
     [SerializeField] private Vector2 respawnTime = Vector2.zero;
+    [SerializeField] private Vector2 speed = Vector2.zero;
     private bool isSpawned = true;
+    private bool isMoving = true;
     private float timeToSpawn = 5f;
     Vector2 spawnPosition = Vector2.zero;
     private Vector2 targetPosition = Vector2.zero;
@@ -22,11 +25,20 @@ public class Cloud : MonoBehaviour
     private void Awake()
     {
         this.anim = GetComponent<Animator>();
+        this.Spawn();
     }
 
     private void Update()
     {
-        if(this.isSpawned) return;
+        if (this.isSpawned)
+        {
+            if (isMoving)
+            {
+                this.MoveTo();
+            }
+
+            return;
+        }
         
         this.timeToSpawn -= Time.deltaTime;
         if (this.timeToSpawn <= 0f)
@@ -40,9 +52,15 @@ public class Cloud : MonoBehaviour
         this.spawnPosition.x = Random.Range(spawnXBounds.x, spawnXBounds.y);
         this.spawnPosition.y = Random.Range(spawnYBounds.x, spawnYBounds.y);
         this.transform.position = spawnPosition;
-        this.anim.SetTrigger("Spawn");
         this.isSpawned = true;
+        this.isMoving = true;
+        this.TargetPosition();
+        this.anim.ResetTrigger("Die");
+        this.anim.SetTrigger("Spawn");
+    }
 
+    private void TargetPosition()
+    {
         do
         {
             this.targetPosition.x = Random.Range(spawnXBounds.x, spawnXBounds.y);
@@ -52,16 +70,27 @@ public class Cloud : MonoBehaviour
 
     private void MoveTo()
     {
-        //@TODO move to target based with coroutine
+        float step = Random.Range(this.speed.x, this.speed.y) * Time.deltaTime;
+        this.transform.position = Vector2.MoveTowards(this.transform.position, this.targetPosition, step);
+        if (Vector2.Distance(this.transform.position, this.targetPosition) < 0.01f)
+        {
+            this.Puddle();
+        }
     }
 
     private void Puddle()
     {
-        //@TODO spawn puddle then die
+        this.isMoving = false;
+        GameObject puddle = Instantiate(this.puddle);
+        puddle.transform.position = this.puddleSpawnPosition.position;
+        StartCoroutine(this.Die());
     }
 
-    private void Die()
+    private IEnumerator Die()
     {
+        yield return new WaitForSeconds(2);
+
+        this.anim.ResetTrigger("Spawn");
         this.anim.SetTrigger("Die");
         this.timeToSpawn = Random.Range(this.respawnTime.x, this.respawnTime.y);
         this.isSpawned = false;
